f4d730b4ff9a20b479a8793bd1a9a811877dd929
BenjaIGVM <115327548+BenjaIGVM@users.noreply.github.com>
2023-10-16 16:44:28 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-//import './index.css'
+import { render } from 'preact'
+import { App } from './app.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+render(<App />, document.getElementById('app'))
